d3d0a94d75057cf3d2731418d1782f5b31520c17
genharding <genharding38@gmail.com>
2026-09-19 10:10:33 -0400

    Add /search to call hirebase api

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from dbm import sqlite3
+from werkzeug.wsgi import responder
 import os
+import requests
 from flask import render_template
 from flask import Flask
 
@@ -10,6 +12,24 @@ app = Flask(__name__, template_folder=os.path.abspath("../frontend/templates"))
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/search')
+def search():
+    url = "https://api.hirebase.org/v2/jobs/search"
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": "hb_d49dd728-6be9-4757-89bd-6a850ea41c2c",
+    }
+    body = {
+        "job_titles": ["Software Engineer"],
+        "keywords": ["Python"],
+        "limit": 10,
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+    data = response.json()
+
+    return data
 
 connect = sqlite3.connect('database.db')
 
