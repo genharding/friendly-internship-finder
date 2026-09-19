517164a5858cf2987aee3fa1135a4940c0852063
genharding <genharding38@gmail.com>
2026-09-19 10:42:42 -0400

    add post method to search

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 from dbm import sqlite3
+from pika.spec import methods
 from werkzeug.wsgi import responder
 import os
 import requests
-from flask import render_template
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Initialize the Flask application
 app = Flask(__name__, template_folder=os.path.abspath("../frontend/templates"))
@@ -13,8 +13,12 @@ app = Flask(__name__, template_folder=os.path.abspath("../frontend/templates"))
 def home():
     return render_template("index.html")
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
+    if request.method == 'POST':
+        keywords = request.form.get('keyword')
+        return 'waow it work'
+
     url = "https://api.hirebase.org/v2/jobs/search"
     headers = {
         "Content-Type": "application/json",
