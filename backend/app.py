b3813aac5a472a9ddfe69a6b8ee205c9d50945ae
genharding <genharding38@gmail.com>
2026-09-19 09:09:31 -0400

    Add template folder to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
+import os
+from flask import render_template
 from flask import Flask
 
 # Initialize the Flask application
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.abspath("../internship-searcher-other/templates"))
 
 # Define a route for the homepage
 @app.route('/')
 def home():
-    return "Hello, World! Your Flask app is running."
+    return render_template("index.html")
 
 # Run the application
 if __name__ == '__main__':
